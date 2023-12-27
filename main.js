04b50fc981fc2f8e390c520ff04d3a6867bf0472
AnarBabazade <babazadeanar94@gmail.com>
2023-12-28 02:21:22 +0400

    innersial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+let a=prompt("Ilin ayini daxil edin")
+switch(a){
+    case "Yanvar" :{
+        console.log(a,"soyuq aydir")
+        break;
+    }
+    case "Fevral" :{
+        console.log(a,"ilin ikinci ayidir");
+        break;
+    }
+    case "Mart" :{
+        console.log(a,"yazin ilk ayidir");
+        break;
+    }
+    case "Aprel" :{
+        console.log(a,"yazin ikinci ayidir");
+        break;
+    }
+    case "May" :{
+        console.log(a,"yazin sonuncu ayidir");
+        break;
+    }
+    case "Iyun" :{
+        console.log(a,"yayin ilk ayidir");
+        break;
+    }
+    case "Iyul" :{
+        console.log(a,"deniz movsumu acilir");
+        break;
+    }
+    case "Avqust" :{
+        console.log(a,"yayin sonuncu ayidir");
+        break;
+    }
+    case "Sentyabr" :{
+        console.log(a,"-da mektebler acilir");
+        break;
+    }
+    case "Oktyabr" :{
+        console.log(a,"yagislar olan aydir");
+        break;
+    }
+    case "Noyabr" :{
+        console.log(a,"havalar soyumaga baslayir");
+        break;
+    }
+    case "Dekabr" :{
+        console.log(a,"ilin sonuncu ayidir");
+        break;
+    }
+    default : {
+        console.log("Ilin bele ayi yoxdur get aylari oyren gel");
+    }
+    
+
+}
